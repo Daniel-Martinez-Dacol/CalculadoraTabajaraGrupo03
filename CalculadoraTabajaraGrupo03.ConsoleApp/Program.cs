2cc1bb6c60e52cab71989acee334ecfb65117d70
Daniel <danieldaol99@gmail.com>
2022-06-15 16:12:01 -0300

    Adicionado visualizar histórico no menu.

--- CalculadoraTabajaraGrupo03.ConsoleApp/Program.cs
+++ CalculadoraTabajaraGrupo03.ConsoleApp/Program.cs
@@ -15,8 +15,10 @@ namespace CalculadoraTabajaraGrupo03.ConsoleApp
                 Console.WriteLine("Calculadora Tabajara do Grupo 03 1.0...\n");
                 Console.WriteLine("Tela principal\n");
                 Console.WriteLine("Digite 1 para realizar operações de Adição\n");
-                Console.WriteLine("Digite 2 para realizar operações de Subtração\n");
-                Console.WriteLine("Digite 3 para realizar operações de Multiplicação\n");
+                Console.WriteLine("Digite 2 para realizar operações de Adição\n");
+                Console.WriteLine("Digite 3 para realizar operações de Multiplicação\n");  
+                Console.WriteLine("Digite 4 para realizar operações de Divisão\n");
+                Console.WriteLine("Digite 5 para visualizar histórico");
                 Console.WriteLine("Digite S para sair\n");
                 Console.WriteLine("Opção: ");
                 opcao = Console.ReadLine();
